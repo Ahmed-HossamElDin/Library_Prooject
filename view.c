9cac8a351b0b97be760233351aa36095d3a33051
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2017-12-26 01:36:03 +0200

    colors

--- view.c
+++ view.c
@@ -1,5 +1,12 @@
 #include "structs.h"
 #include "actions.h"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 void view_current_time()
 {
             time_t rawtime;
@@ -9,21 +16,21 @@ void view_current_time()
             current_time.day=timeinfo->tm_mday;
             current_time.month=timeinfo->tm_mon + 1;
             current_time.year=timeinfo->tm_year + 1900;
-            printf("                                        Today's date is : %d/%d/%d\n",current_time.day,current_time.month,current_time.year);
+            printf(ANSI_COLOR_CYAN"                                        Today's date is : %d/%d/%d\n"  ANSI_COLOR_RESET,current_time.day,current_time.month,current_time.year);
 
 }
 void view_main_menu()
 { /*views main menu options*/
 
-    printf("\t\t\t\t\t!!!!!!!!!WELCOME>>>>>>>>>>>>>\n");
+    printf(ANSI_COLOR_RED  "\t\t\t\t\t!!!!!!!!!WELCOME>>>>>>>>>>>>>\n"   ANSI_COLOR_RESET "\n");
 
-printf("(1) Book Management\n(2) Member Management\n(3) Borrow Management\n(4) Administrative actions\n(5) Save Changes\n(6) View all books\n(7) View all members\n(8) Exit\n");}
+printf(ANSI_COLOR_GREEN"(1) Book Management\n(2) Member Management\n(3) Borrow Management\n(4) Administrative actions\n(5) Save Changes\n(6) View all books\n(7) View all members\n(8) Exit\n"ANSI_COLOR_RESET);}
 
 void view_menu_book_management()
 {/*views view menu options*/
     system("cls");
 
-    printf("(1) Insert a new book\n(2) Search for a book\n(3) Add New Copy\n(4) Delete book\n(5) Back\n");
+    printf(ANSI_COLOR_YELLOW"(1) Insert a new book\n(2) Search for a book\n(3) Add New Copy\n(4) Delete book\n(5) Back\n"ANSI_COLOR_RESET);
 
 
 }
